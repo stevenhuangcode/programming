e222d64afa749fe0a22070578e7ede5efe0deb33
Steven <stevenhuangpro@gmail.com>
2021-01-13 09:45:17 -0800

    dvd start

---
+++ pygame/dvd_image_moving.py
@@ -0,0 +1,54 @@
+# dvd_image_moving.py
+# create a rectangle that moves around the screen
+
+# goals:
+# create a rectangle class
+# size and position
+# colour
+# velocity x/y direction
+
+import pygame
+
+# ----- CONSTANTS
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
+SKY_BLUE = (95, 165, 228)
+WIDTH = 800
+HEIGHT = 600
+TITLE = "DVD Screensaver"
+
+
+def main():
+    pygame.init()
+
+    # ----- SCREEN PROPERTIES
+    size = (WIDTH, HEIGHT)
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption(TITLE)
+
+    # ----- LOCAL VARIABLES
+    done = False
+    clock = pygame.time.Clock()
+
+    # ----- MAIN LOOP
+    while not done:
+        # -- Event Handler
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+
+        # ----- LOGIC
+
+        # ----- DRAW
+        screen.fill(BLACK)
+
+        # ----- UPDATE
+        pygame.display.flip()
+        clock.tick(60)
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
